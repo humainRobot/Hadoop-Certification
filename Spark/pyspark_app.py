b0ea527488bda380360f14849195721f1b92bf2e
humainRobot <anes.mahi@gmail.com>
2016-05-01 11:00:02 -0700

    Update pyspark_app.py

--- Spark/pyspark_app.py
+++ Spark/pyspark_app.py
@@ -1,13 +1,12 @@
-#Developing simple scala based applications for spark
-#Save this to a file with py extension
 
 from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setAppName("pyspark")
 sc = SparkContext(conf=conf)
-dataRDD = sc.textFile("/user/cloudera/sqoop_import/departments")
+dataRDD = sc.textFile("/user/cloudera/sqoop_import/retail.db/departments")
 for line in dataRDD.collect():
     print(line)
+
 dataRDD.saveAsTextFile("/user/cloudera/pyspark/departmentsTesting")
 
 #Run using this command
