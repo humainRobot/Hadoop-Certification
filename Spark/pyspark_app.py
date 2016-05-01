60e33b931c6401c3eca7d7440293b4834d8f63e7
humainRobot <anes.mahi@gmail.com>
2016-05-01 11:58:02 -0700

    Update pyspark_app.py

--- Spark/pyspark_app.py
+++ Spark/pyspark_app.py
@@ -9,6 +9,7 @@ dataRDD = sc.textFile("/user/cloudera/sqoop_import/departments")
 for line in dataRDD.collect():
     print(line)
 dataRDD.saveAsTextFile("/user/cloudera/spark/pyspark/departmentsTesting")
+
 #Run using this command
 #master local will run in spark native mode : spark-submit --master local pyspark_app.py
 #master yarn will run in yarn mode : spark-submit --master yarn pyspark_app.py
